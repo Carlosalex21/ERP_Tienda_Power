c3402fa585e211597ffaa45283dbd99b8f6ba37b
Carlosalex21 <thelucars21@gmail.com>
2025-03-31 23:28:38 -0400

    cambios en campos de la bd y views de productos

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "erp_tienda",
 ]
 
 MIDDLEWARE = [
@@ -112,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = "es-us"
+LANGUAGE_CODE = "es-es"
 
 TIME_ZONE = "UTC"
 
